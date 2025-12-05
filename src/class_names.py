9dd7fc64323dd7c41f4321f2de92974f8f0441a7
wenebifid <162278055+wenebifid@users.noreply.github.com>
2025-12-05 14:02:30 +0100

    label names

---
+++ src/class_names.py
@@ -0,0 +1,12 @@
+CLASS_NAMES = [
+    "AnnualCrop",
+    "Forest",
+    "HerbaceousVegetation",
+    "Highway",
+    "Industrial",
+    "Pasture",
+    "PermanentCrop",
+    "Residential",
+    "River",
+    "SeaLake"
+]
